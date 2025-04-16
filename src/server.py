01e0a23e80851c43cf002049132f7b2a38eb407e
butterflyforkill <vasse.paul@gmail.com>
2025-04-16 15:18:59 +0200

    fix(routes): fix for imports and routes naming, checked running

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
-from .webhook_service.google_calendar.routes import gcal_web_hook_router
-from .webhook_service.slack.routes import slack_web_hook_router
+from google_calendar.routes import gcal_web_hook_router
+from slack.routes import slack_web_hook_router
 
 
 app = FastAPI(title="MindGuard Webhook Service")
@@ -11,9 +11,9 @@ app.include_router(slack_web_hook_router, tags=["Slack"])
 app.include_router(gcal_web_hook_router, tags=["Google Calendar"])
 
 
-@app.get("/hello")
-async def hello_world():
-    return "Hello, World!"
+# @app.get("/hello")
+# async def hello_world():
+#     return "Hello, World!"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
--- src/slack/routes.py
+++ src/slack/routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-
 from .schemas import  SlackEventCallback
+
 
 slack_web_hook_router = APIRouter()
 
-@slack_web_hook_router.post("webhook-events/slack")
+
+@slack_web_hook_router.post("/webhook-events/slack")
 async def handle_slack_webhook(payload:SlackEventCallback):
     return {"status": "success", "code": 200, "message": "Webhook slck event received"}
